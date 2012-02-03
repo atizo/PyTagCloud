fde06a5c29bce4583061de398de2dd75d800ed96
Reto Aebersold <aeby@atizo.com>
2012-02-03 14:18:26 +0100

    adjust setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(
     description = "Create beautiful tag clouds as images or HTML",
     author = 'Reto Aebersold',
     author_email = 'aeby@atizo.com',
-    url = 'https://github.com/atizo/PyTagCloud',
-    package_dir = {'': 'src'},
-    packages = ['pytagcloud'],
+    url = 'https://github.com/atizo/PyTagCloud',    
+    packages = ['pytagcloud', 'pytagcloud.lang'],
+    package_dir = {'pytagcloud': 'src/pytagcloud'},
+    package_data = {'pytagcloud': ['fonts/*.*']},
     classifiers = [
                    'Development Status :: 3 - Alpha',
                    'Environment :: Web Environment',
